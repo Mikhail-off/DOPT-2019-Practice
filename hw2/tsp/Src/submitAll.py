ac89b302e10fdd087874a1be3f6dd5092d69390f
HukuToc46 <hukutoc46@gmail.com>
2019-06-13 03:06:46 +0300

    submittion script

--- hw2/tsp/Src/submitAll.py
+++ hw2/tsp/Src/submitAll.py
@@ -7,16 +7,25 @@ taskName = 'tsp'
 buildPath = os.path.join(solutionDir, hwName,'x64\\Release\\')
 
 solverPath = os.path.join(buildPath, taskName + '.exe')
+submitPath = os.path.join(solutionDir, hwName, taskName, 'Task', 'submit.py')
 
 testsDir = os.path.join(solutionDir, hwName, taskName, 'Task', 'tests')
+
+myName = 'test_name'
 
 def runOnTest(name):
     testName = name + '.public'
     ansName = name + '.answer'
-    cmd = solverPath + ' ' + testName + ' ' + ansName
+    cmd = ' '.join([solverPath, testName, ansName])
+    print(cmd)
     os.system(cmd)
 
-
+def submitTest(name):
+    testName = name + '.public'
+    ansName = name + '.answer'
+    cmd = ' '.join(['python', submitPath, myName, taskName, testName, ansName])
+    print(cmd)
+    os.system(cmd)
 
 def main():
     tests = os.listdir(testsDir)
@@ -25,7 +34,7 @@ def main():
         name, ext = os.path.splitext(test)
         if ext != '.public':
             continue
-
+        name = os.path.join(testsDir, name)
         print('Running ' + test)
         proc = mp.Process(target=runOnTest, args=(name,))
         procs.append(proc)
@@ -33,7 +42,21 @@ def main():
     
     for proc in procs:
         proc.join()
-        
+
+    procs = []
+    for test in tests:
+        name, ext = os.path.splitext(test)
+        if ext != '.answer':
+            continue
+        name = os.path.join(testsDir, name)
+        print('Submit ' + test)
+        proc = mp.Process(target=submitTest, args=(name,))
+        procs.append(proc)
+        proc.start()
+    
+    for proc in procs:
+        proc.join()
+    
 
 if __name__ == '__main__':
     main()
